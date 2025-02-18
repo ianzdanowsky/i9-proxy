567fa1ced944ade425ab448dd7da015de3d4aa4f
Ian Zdanowsky <ianzpimentel@gmail.com>
2025-02-18 18:45:45 -0300

    chore: Add certbot and nginx services to docker-compose.yaml

---
+++ certbot/create-cert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DOMAIN="asm.i9dados.com.br"
+EMAIL="tportela3@gmail.com"
+
+docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot \
+  --email $EMAIL --agree-tos --no-eff-email \
+  --force-renewal -d $DOMAIN -d www.$DOMAIN

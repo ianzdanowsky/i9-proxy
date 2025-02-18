84deae5562850d42f55d033f3abbe233ed745dbe
Ian Zdanowsky <ianzpimentel@gmail.com>
2025-02-18 18:47:40 -0300

    chore: Update certbot command in create-cert.sh

--- certbot/create-cert.sh
+++ certbot/create-cert.sh
@@ -3,6 +3,6 @@
 DOMAIN="asm.i9dados.com.br"
 EMAIL="tportela3@gmail.com"
 
-docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot \
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot \
   --email $EMAIL --agree-tos --no-eff-email \
   --force-renewal -d $DOMAIN -d www.$DOMAIN
